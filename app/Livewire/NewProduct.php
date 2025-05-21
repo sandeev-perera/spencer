c8ed6f8d4f3d97cfd31332c33390fef7fd145b86
Sandeev Perera <sandeev.perera@gmail.com>
2025-05-21 12:15:31 +0530

    v2

--- app/Livewire/NewProduct.php
+++ app/Livewire/NewProduct.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use Illuminate\Support\Facades\Log;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class NewProduct extends Component
@@ -27,21 +28,37 @@ class NewProduct extends Component
     {
         $this->variants[] = [
             'color' => '',
+            'images'=>[''],
+            'sub_variants' =>[
+            [
             'size' => '',
             'stock_quantity' => '',
-            'sku' => '',
-            'images' => [''],
+            'sku' => ''
+            ]
+
+            ]
         ];
     }
 
-    // public function addTag(){
-    //     $this->
-    // }
+    public function addSubVariant($index){
+        $this->variants[$index]["sub_variants"][]=
+        [
+            'size' => '',
+            'stock_quantity' => '',
+            'sku' => ''
+        ]
+        ;
+    }
 
     public function removeVariant($index)
     {
         unset($this->variants[$index]);
         $this->variants = array_values($this->variants);
+    }
+
+    public function removeSubVariant($index, $subindex){
+        unset($this->variants[$index]['sub_variants'][$subindex]);
+        $this->variants[$index]['sub_variants'] = array_values($this->variants[$index]['sub_variants']);
     }
 
     public function render()
@@ -55,6 +72,38 @@ class NewProduct extends Component
 
     public function save()
     {
+       
+        Log::info("Product ID: " . $this->product_id);
+
+        // Store thumbnail image
+        $thumbnailPath = null;
+        if ($this->thumbnail_image) {
+            $directory = "products/{$this->product_id}";
+            Storage::disk('public')->makeDirectory($directory);
+            $thumbnailPath = $this->thumbnail_image->storeAs($directory, 'thumbnail.jpg', 'public');
+            Log::info('Thumbnail image stored at: ' . $thumbnailPath);
+        }
+
+        // Store variant images
+        $variantData = $this->variants;
+        Log::info('Variants data:', $variantData);
+        foreach ($variantData as $index => $variant) {
+            if (!empty($variant['images'])) {
+                $color = trim(str_replace(['/', '\\', '..'], '', $variant['color'] ?? 'unknown'));
+                $variantDirectory = "products/{$this->product_id}/variants/{$color}";
+                Storage::disk('public')->makeDirectory($variantDirectory);
+                Log::info("Variant directory created: " . $variantDirectory);
+                $variantData[$index]['images'] = array_map(function ($image) use ($variantDirectory, $index) {
+                    $filename = uniqid() . '.' . $image->getClientOriginalExtension();
+                    $path = $image->storeAs($variantDirectory, $filename, 'public');
+                    Log::info("Variant image stored at index {$index}: " . $path);
+                    return $path;
+                }, $variant['images']);
+            } else {
+                Log::info("No images for variant at index {$index}");
+            }
+        }
+
         Log::info('Product Data:', [
             'product_id' => $this->product_id,
             'name' => $this->name,
